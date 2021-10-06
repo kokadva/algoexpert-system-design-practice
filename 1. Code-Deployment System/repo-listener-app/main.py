0f38c2fa74d3966a67cb27e744385178bd5acae7
kdvalishivli <kdvalishvili@opus2.com>
2021-10-06 11:53:38 +0400

    Add comments

--- 1. Code-Deployment System/repo-listener-app/main.py
+++ 1. Code-Deployment System/repo-listener-app/main.py
@@ -1,27 +1,40 @@
 import datetime
-import json
 import os
 import random
 import string
-
-import pika
-
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# DB Settings
 SQLALCHEMY_DATABASE_URL = USER = os.getenv('DB_URI', "postgresql://dev:dev@localhost:5432/dev")
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+
+# DB Model
+class DeploymentJobs(Base):
+    __tablename__ = "DEPLOYMENT_JOBS"
+
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String, default='PENDING')
+    commit_code = Column(String, default='')
+    last_modified = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+def create_tables():
+    DeploymentJobs.__table__.create(engine)
+
+# Once run creates tables in the DB
+# create_tables()
+
+# FastAPI Init
 app = FastAPI()
 
+# Cors settings
 origins = [
     "*",
 ]
@@ -35,21 +48,16 @@ app.add_middleware(
 )
 
 
-class DeploymentJobs(Base):
-    __tablename__ = "DEPLOYMENT_JOBS"
-
-    id = Column(Integer, primary_key=True, index=True)
-    status = Column(String, default='PENDING')
-    commit_code = Column(String, default='')
-    last_modified = Column(DateTime, default=datetime.datetime.utcnow)
-
-
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-@app.get("/test")
-async def test():
-    return {"message": "test"}
+
+# Endpoints
+
+@app.get("/ping")
+async def ping():
+    return {"message": "repo-listener <3"}
+
 
 @app.post("/repo-webhook")
 async def root(request: Request):
@@ -64,10 +72,3 @@ async def root(request: Request):
         "commit_code": deployment_job.commit_code,
         "last_modified": deployment_job.last_modified.__str__()
     }
-
-
-def create_tables():
-    DeploymentJobs.__table__.create(engine)
-
-
-# create_tables()
