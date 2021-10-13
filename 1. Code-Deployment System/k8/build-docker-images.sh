1eae53886917af2924a64a7cc62f65bb62c6b409
kdvalishivli <kdvalishvili@opus2.com>
2021-10-13 19:24:59 +0400

    Add images pull

--- 1. Code-Deployment System/k8/build-docker-images.sh
+++ 1. Code-Deployment System/k8/build-docker-images.sh
@@ -12,4 +12,10 @@ docker build -t worker-app ../worker-app/
 echo "Building client-app image"
 docker build -t client-app ../client-app/
 
+echo "Pulling imges from dockerhub"
+docker pull gitea/gitea:1.15.3
+docker pull postgres:9.6
+docker pull minio/minio
+
+
 echo "Docker images build complete"
